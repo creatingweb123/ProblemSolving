b472e60256045baabe6a48405b4fdbb55c11a43a
creatingweb123 <shegegegey1@gmail.com>
2025-09-09 16:51:00 +0900

    feat: goormthon monday challenge 1-2

---
+++ src/Problem2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Problem2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        String name = input[1];
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            String studentName = br.readLine();
+            if (studentName.contains(name)) {
+                result += 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
