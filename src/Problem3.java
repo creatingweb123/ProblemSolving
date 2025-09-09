9b02183231cbeb1f180ae4478d0030d5a59bf731
creatingweb123 <shegegegey1@gmail.com>
2025-09-09 17:01:28 +0900

    feat: goormthon monday challenge 1-3

---
+++ src/Problem3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Problem3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int[] vList = new int[4];
+        for (int i = 0; i < 4; i++) {
+            vList[i] = Integer.parseInt(input[i]);
+        }
+        Arrays.sort(vList);
+        int result = vList[3] + vList[2] - vList[1] - vList[0];
+        System.out.println(result);
+    }
+}
