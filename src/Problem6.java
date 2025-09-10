8fc9d3681f7d92834871e87b029e8f07c2b5ccc7
creatingweb123 <shegegegey1@gmail.com>
2025-09-10 11:36:35 +0900

    feat: goormthon monday challenge 2-2

---
+++ src/Problem6.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Problem6 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+
+        char beforeChar = 0;
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            if (beforeChar != S.charAt(i)) {
+                beforeChar = S.charAt(i);
+                result += 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
